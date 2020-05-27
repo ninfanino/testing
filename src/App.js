d4905e1d6f782e36b0ca856d8a0bd46759b8b39a
Ninfa Niño <ninfa_sys@hotmail.com>
2020-05-26 21:40:26 -0500

    initial commit of componenttesting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/header/Header';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <Header/>
     </div>
   );
 }
